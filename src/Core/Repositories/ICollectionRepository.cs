3040f0300ea688e390c0f12f8cdb18b1550c1a45
Jacob Fink <jfink@bitwarden.com>
2022-10-12 15:40:24 -0400

    [EC-14] add collection repo methods to interface

--- src/Core/Repositories/ICollectionRepository.cs
+++ src/Core/Repositories/ICollectionRepository.cs
@@ -9,11 +9,14 @@ public interface ICollectionRepository : IRepository<Collection, Guid>
     Task<Tuple<Collection, ICollection<SelectionReadOnly>>> GetByIdWithGroupsAsync(Guid id);
     Task<Tuple<CollectionDetails, ICollection<SelectionReadOnly>>> GetByIdWithGroupsAsync(Guid id, Guid userId);
     Task<ICollection<Collection>> GetManyByOrganizationIdAsync(Guid organizationId);
+    Task<ICollection<Tuple<Collection, ICollection<SelectionReadOnly>>>> GetManyWithGroupsByOrganizationIdAsync(Guid organizationId);
     Task<CollectionDetails> GetByIdAsync(Guid id, Guid userId);
+    Task<ICollection<Collection>> GetManyByManyIds(IEnumerable<Guid> collectionIds);
     Task<ICollection<CollectionDetails>> GetManyByUserIdAsync(Guid userId);
     Task CreateAsync(Collection obj, IEnumerable<SelectionReadOnly> groups);
     Task ReplaceAsync(Collection obj, IEnumerable<SelectionReadOnly> groups);
     Task DeleteUserAsync(Guid collectionId, Guid organizationUserId);
     Task UpdateUsersAsync(Guid id, IEnumerable<SelectionReadOnly> users);
     Task<ICollection<SelectionReadOnly>> GetManyUsersByIdAsync(Guid id);
+    Task DeleteManyAsync(Guid organizationId, IEnumerable<Guid> collectionIds);
 }
